8af0aaa6dca8ea7cb14d9bb71b84386ecaf819e2
= <=>
2020-10-23 21:10:47 +0800

    fix

--- hw4/4C/binary_tree.c
+++ hw4/4C/binary_tree.c
@@ -1,8 +1,23 @@
 #include "list.h"
 
+struct tree_node {
+    int val;
+    struct list_head left;
+    struct list_head right;
+};
 
-int main() {
-    printf("ok\n");
+
+int main(int argc, char* argv[]) {
+    struct tree_node root;
+    struct tree_node *tmp;
+    INIT_LIST_HEAD(&(root.left));
+    INIT_LIST_HEAD(&(root.right));
+    for(int i = 0; i < 10; ++i) {
+        tmp = (struct person *)malloc(sizeof(struct tree_node));
+        tmp->val = i;
+        list_add(&(tmp->left), &(root.left));
+    }
+    return 0;
 }
 
 
